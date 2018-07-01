42c3bea5f9f4b627b25306de6abec23675cb4f03
Chiyeti Harishkumar Reddy <creddy08@in.ibm.com>
2018-07-01 14:37:03 +0530

    remove

--- spring-boot-demo/src/main/java/com/sriharish/springbootdemo/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/sriharish/springbootdemo/SpringBootDemoApplication.java
@@ -8,7 +8,7 @@ public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
-		System.out.println("Hello Git");
+		System.out.println("Git");
 		System.out.println("Hello Updated Git Code");
 	}
 }
